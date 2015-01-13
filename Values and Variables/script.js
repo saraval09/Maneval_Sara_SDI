fa8caca05b6bb87540c00e5dcf0e2861dcdd50b9
saraval09 <saramaneval@Saras-MacBook-Pro.local>
2015-01-12 21:09:40 -0500

    Deleted folder/ Added new folder
    Deleted the extra Maneval_Sara_SDI folder and added the folder “Values
    and Variables” that has a script file and html file in it.

---
+++ Values and Variables/script.js
@@ -0,0 +1,30 @@
+/*var yearBorn = 1955;
+console.log("Hello World!");
+console.log(yearBorn);
+alert (yearBorn);
+*/
+
+//Defining Variables
+/*var age; //declaration
+age = 24; //definition
+//storage container = value
+
+var yearOfBirth = 1990; // declaring and defining on the same line
+*/
+
+/* Variables of Different Types, Number, Booleans, and Strings
+
+var age = 24; // Number variable
+
+var isStudent = false; // Boolean Variable
+
+var firstName= "Sara";
+var phrase = 'I don\'t know \n You\'ll have to speak with the professor';*/
+
+// Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; // 0,1,2,3
+muppetNames[0]="Animal";
+muppetNames[4]="Fozzy";
+var num = 2;
+console.log(muppetNames[num]);
