d5510839cf3850e676df3527ed0c2900dad57763
saraval09 <saramaneval@Saras-MacBook-Pro.local>
2015-01-14 19:37:00 -0500

    Calculated age
    Talked about using conditionals in the future

--- GoToTraining2/js/gotoTraining2.js
+++ GoToTraining2/js/gotoTraining2.js
@@ -14,3 +14,20 @@ var name = prompt("Please type in your name:");
 console.log(name);
 
 alert("Welcome " +name+ "! Let's figure out how old you are.")
+
+//Ask the user what year they are born
+// age = current year subtract the year they were born
+//Create a variable to catch the prompted answer
+
+var yearBorn = prompt("What year were you born:");
+console.log(yearBorn);
+
+//What is the current Year?
+var currentYear = 2015;
+
+// calculate age - create a var for results
+var age = currentYear-yearBorn-1;
+console.log(name+" you are "+age+" years old.");
+
+alert(name+" you are "+age+" years old.");
+
