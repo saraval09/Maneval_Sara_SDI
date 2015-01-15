58f3adb43ec065a193525b8442bfe3fdc2bd1f6a
saraval09 <saramaneval@Saras-MacBook-Pro.local>
2015-01-14 19:29:09 -0500

    prompted user for name
    Used a prompt to get users name

--- GoToTraining2/js/gotoTraining2.js
+++ GoToTraining2/js/gotoTraining2.js
@@ -6,5 +6,11 @@ GoTo Training Day #2
  */
 //alert("Testing to see if the JS file is attached to the HTML.");
 
+// Create an age calculator
 
+//Ask the user their name
+var name = prompt("Please type in your name:");
 
+console.log(name);
+
+alert("Welcome " +name+ "! Let's figure out how old you are.")
