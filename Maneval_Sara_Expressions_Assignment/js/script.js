522b81ef4d8a6a3ee84862d0c409b91c84f1d204
saraval09 <saramaneval@Saras-MacBook-Pro.local>
2015-01-15 22:08:15 -0500

    Added JS file and HTML file
    Added a JS and HTML file. Made sure they are linked. Started my
    expression assignment.

---
+++ Maneval_Sara_Expressions_Assignment/js/script.js
@@ -0,0 +1,16 @@
+/*
+ Sara Maneval
+ SDI Section #3
+ Expression Assignment
+ 1/15/2015
+ */
+//alert("Testing to see if the JS file is attached to the HTML.");
+
+var name = prompt("What is your name?");
+
+console.log(name);
+
+alert("Hello " +name+ "! I have a few questions to ask you about movies.")
+
+var monthlyMovies = prompt("How many movies do you see at a theatre a month?");
+console.log(monthlyMovies);
