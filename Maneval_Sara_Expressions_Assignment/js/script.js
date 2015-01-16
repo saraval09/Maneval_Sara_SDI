37da310b64da4fffaae8cb6daca441bf14c83735
saraval09 <saramaneval@Saras-MacBook-Pro.local>
2015-01-15 22:59:44 -0500

    Casting
    Figured problem out with the casting and line breaks

--- Maneval_Sara_Expressions_Assignment/js/script.js
+++ Maneval_Sara_Expressions_Assignment/js/script.js
@@ -20,11 +20,11 @@ var monthlyMovies = prompt("How many movies do you see at a theatre a month?");
 console.log(monthlyMovies);
 
 //Find out how much they pay for a movie ticket
-var ticketCost = prompt("About how much is the movie ticket?");
+var ticketCost = prompt("About how much is the movie ticket?\nOnly enter numbers please!");
 console.log(ticketCost);
 
 //Find out how much they spend on snacks
-var snackCost = prompt("About how much do you spend on snacks?");
+var snackCost = prompt("About how much do you spend on snacks?\nOnly enter numbers please!");
 console.log(snackCost);
 
 //Thank them using their name
@@ -37,11 +37,12 @@ alert("I am now calculating about how much you spend monthly at a movie theatre!
 alert("Almost done! I bet if you click “OK” I will know the answer!");
 
 //Find the out how much they spend by adding the cost of the tickets and snacks and then multiplying them out by how many times they see a movie a month
-var totalCost = (ticketCost+snackCost) * monthlyMovies;
+var totalCost =( Number(ticketCost)+Number(snackCost)) * monthlyMovies;
 console.log(totalCost);
 
 //Let the user know how much they spend at the theatre monthly
-alert("You spend "+totalCost+ " monthly at the theatre!");
+alert("You spend $"+totalCost+ " a month at the theatre!");
 
 //Thank the user for their time
 alert("Thank you for your time "+name+ "!"+ " " + "Have a wonderful day!");
+
