e9a4b1d0eea0e72f2e9a92cb446d5d601da7ea27
saraval09 <saramaneval@Saras-MacBook-Pro.local>
2015-01-21 22:04:51 -0500

    Conditional Worksheet Group 3
    Finished Group 3 Tire Pressure. Had to ask Dan what single condition
    meant.

--- Conditionals_worksheet/Grade Letter Calculator-Group 2/js/script.js
+++ Conditionals_worksheet/Grade Letter Calculator-Group 2/js/script.js
@@ -9,15 +9,27 @@ Conditionals_worksheet_Grade Letter Calculator
 //A student earns a number grade at the conclusion of a course at Full Sail.  Determine the appropriate letter grade for that number using conditional statements.
 
 
-var mpg =25 ;
-var gauge =.50;
-var tank=15;
-var left= tank*gauge;
+var grade= 97;
 
 
-if(mpg*left-200<=200){
-    console.log("You only have "+ left + " gallons of gas in your tank, better get gas now while you can!");
-}else{
-    console.log("Yes, you can make it without stopping for gas!");
+//Test what grade- norma
+
+//A- 100-90
+//B- 89-80
+//C 79-70
+//D 69-60
+//F 59- 0
+
+if(grade>90){
+    console.log("You have a "+grade+"%, which means you have earned a(n) A in the class!");
+
+}else if (grade>80){
+    console.log("You have a "+grade+"%, which means you have earned a(n) B in the class!");
+
+}else if (grade>70){
+    console.log("You have a "+grade+"%, which means you have earned a(n) C in the class!");
+}else if (grade>60){
+    console.log("You have a "+grade+"%, which means you have earned a(n) D in the class!");
+}else {
+    console.log("You have a "+grade+"%, which means you have earned a(n) F in the class!");
 }
-
--- Conditionals_worksheet/Tire Pressure-Group 3/js/script.js
+++ Conditionals_worksheet/Tire Pressure-Group 3/js/script.js
@@ -8,16 +8,14 @@ Conditionals_worksheet_Tire Pressure
 
 //To meet to maintenance standards a car’s front two tires should have the same pressure and the back two tires should have the same pressure.  But the front tires and the rear tires can have different pressure than each other, so it is not necessary for all four tires’ pressure to be the same. Create a single conditional that would determine if the tires of a given car are up to spec. 
 
+var frontA= 25;
+var frontB= 35;
+var backA= 40;
+var backB= 40;
 
-var mpg =25 ;
-var gauge =.50;
-var tank=15;
-var left= tank*gauge;
+if( frontA ===frontB && backA ===backB) {
+   console.log("The tires pass spec!");
 
-
-if(mpg*left-200<=200){
-    console.log("You only have "+ left + " gallons of gas in your tank, better get gas now while you can!");
 }else{
-    console.log("Yes, you can make it without stopping for gas!");
+   console.log("Get your tires check out!");
 }
-
