34e9660176367c46d82e75efd03e92e1bceb5bf6
saraval09 <saramaneval@Saras-MacBook-Pro.local>
2015-01-21 19:21:52 -0500

    Added GoTo Training Folder
    Learned index numbers

---
+++ GoTo Training 3/js/GoToDay3.js
@@ -0,0 +1,25 @@
+/*
+Sara Maneval
+1/21/2015
+SDI Section #3
+GoTo Training Day #3
+ */
+
+//Use an alert to test if the js file is connected to the HTML
+
+//alert("Testing to see if connected.");
+
+//Check how many pears are in a fruit bowl
+//Create out "Fruit bowl"
+//Use an array, variable that holds multiple variables
+
+var bowlOfFruit = ["apple","pear","peach"];
+console.log(bowlOfFruit);
+
+//Find out how many items are in an array
+//length property - items in array
+//Dot Syntax -"use a period"
+console.log(bowlOfFruit.length);
+
+//Call upon a specific item in the array
+console.log(bowlOfFruit[2]);
